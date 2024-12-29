349c154b8b322673a333dbade82d5d5e6ad296a7
emaduddin678 <emaduddin678.com>
2024-12-29 12:16:57 +0600

    login page done

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -38,6 +38,20 @@ const useAuthStore = create((set) => ({
       set({ isSigningUp: false });
     }
   },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data.payload.user });
+      toast.success("Logged in successfully");
+
+      // get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
   logout: async () => {
     try {
       await axiosInstance.post("/auth/logout");
@@ -46,6 +60,19 @@ const useAuthStore = create((set) => ({
       // get().disconnectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
+    }
+  },
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
     }
   },
 }));
